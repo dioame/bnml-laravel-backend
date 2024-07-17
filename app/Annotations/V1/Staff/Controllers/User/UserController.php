c2b5e38873b63645ec39f6a6c0a7e363931f8c5a
dioamejade@gmail.com <dioamejade@gmail.com>
2024-07-17 12:54:38 +0800

    feature/23

--- app/Annotations/V1/Staff/Controllers/User/UserController.php
+++ app/Annotations/V1/Staff/Controllers/User/UserController.php
@@ -59,4 +59,64 @@ class UserController extends Controller
      * )
      */
 
+
+     /**
+     * @OA\Get(
+     *      path="/user/{id}",
+     *      tags={"User"},
+     *      summary="Staff user details by id",
+     *      description="Returns user details including first name, middle name, last name, birth date, email, and mobile number.",
+     *  *     @OA\Parameter(
+ *         name="id",
+ *         in="path",
+ *         description="ID of the user",
+ *         required=true,
+ *     ),
+     *    @OA\Response(
+     *          response="200",
+     *          description="OK",
+     *          @OA\JsonContent(
+    example= {
+  "status": "success",
+  "description": "OK",
+  "data": {
+    "first_name": "test1",
+    "middle_name": "test2",
+    "last_name": "test3",
+    "birth_date": "1990-01-01",
+    "email": "test@test.com",
+    "mobile": "09000000000"
+  }
+}
+     *          )
+     *      ),
+     *     @OA\Response(
+     *          response="401",
+     *          description="Unauthorized",
+     *          @OA\JsonContent(
+    example= {
+    "status": "error",
+    "description": "Unauthorized"
+    },
+     *          )
+     *      ),
+     *     @OA\Response(
+     *          response="403",
+     *          description="Forbidden",
+     *          @OA\JsonContent(
+     *                  example= {
+    "status": "error",
+    "description": "Forbidden",
+    "errors": {
+    {
+    "code": 3004,
+    "message": "Staff inactive"
+    },
+    }
+    },
+     *          )
+     *      ),
+     * )
+     */
+
 }
--- app/Http/Controllers/Api/V1/Staff/Users/UserController.php
+++ app/Http/Controllers/Api/V1/Staff/Users/UserController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Services\V1\Staff\Users\UserService;
+use App\Services\V1\Staff\Users\UserServiceBy;
 use App\Http\Resources\Staff\Users\UserCollection;
+use App\Http\Resources\Staff\Users\UserResource;
 
 
 class UserController extends Controller
@@ -16,5 +18,10 @@ class UserController extends Controller
         $result = $service->execute();
 
         return new UserCollection($result);
+    }
+
+    public function show(UserServiceBy $service, $id){
+        $result = $service->execute($id);
+        return new UserResource($result);
     }
 }
