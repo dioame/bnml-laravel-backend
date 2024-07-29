e35f3716b15575e7015b32749a0b1c37a071c4d8
dioamejade@gmail.com <dioamejade@gmail.com>
2024-07-29 09:23:23 +0800

    fix-error

--- app/Http/Controllers/Api/V1/Staff/SummaryPoints/SummaryPointsController.php
+++ app/Http/Controllers/Api/V1/Staff/SummaryPoints/SummaryPointsController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Services\V1\Staff\SummaryPoints\SummaryPointsService;
-use App\http\Resources\Staff\SummaryPoints\SummaryPointsCollection;
+use App\Http\Resources\Staff\SummaryPoints\SummaryPointsCollection;
 
 
 
